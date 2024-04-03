88248fd99feda5b4332db4ff8ba5e3d8d159b84e
ADMIN <ADMIN@TGBLR024>
2024-04-03 08:58:54 +0530

    First Demo Sample

---
+++ MainDemo/src/test/java/com/MainDemo/demo/MainDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MainDemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
